d2905d20a7ef49b009844b2070706fa9aea5ed84
Mark Goertz <434548@student.fontys.nl>
2022-11-19 00:03:44 +0100

    Update TestController.cs

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -2,13 +2,24 @@
 
 namespace MovieMicroservice.Controllers
 {
+        
+    
     [Route("/")]
     [ApiController]
     public class TestController : Controller
     {
+        private readonly IConfiguration _configuration;
+        public TestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public string Index()
         {
-            return "Hello World!";
+            var baseURL = _configuration.GetValue<string>("API:BaseURL");
+            var API_key = _configuration.GetValue<string>("API:API_key");
+            var url = baseURL + API_key;
+            
+            return url;
         }
     }
 }
